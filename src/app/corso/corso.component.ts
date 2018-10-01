83967e71fc3e4c75a9abd2189e424878f1dcf53d
Daniele <daniele.gallesio@email.it>
2018-10-01 14:02:01 +0200

    daniele_wip_inizio

---
+++ src/app/corso/corso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-corso',
+  templateUrl: './corso.component.html',
+  styleUrls: ['./corso.component.css']
+})
+export class CorsoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
