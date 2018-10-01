53a88df365a7f4316a6132247f5ff23d0c6d8c40
marco.roccasuits92 <marco.roccasuits92@gmail.com>
2018-10-01 14:02:59 +0200

    cartella docenti + inizio modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DocentiComponent } from './docenti/docenti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocentiComponent
   ],
   imports: [
     BrowserModule
