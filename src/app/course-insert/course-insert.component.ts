2fe9d2715dd9912aa91f56958af73f75998521ae
Daniele <daniele.gallesio@email.it>
2018-10-10 10:33:06 +0200

    Inserimento corsi funge e naviga al corso appena inserito

--- src/app/course-insert/course-insert.component.ts
+++ src/app/course-insert/course-insert.component.ts
@@ -20,19 +20,28 @@ export class CourseInsertComponent implements OnInit {
 
   ngOnInit() :void {
     this.route.params.subscribe( params => console.log(params) );
+    console.log(this.router)
   }
 
   conferma(myFirstNgForm :NgForm){
     console.log(myFirstNgForm);
     
     let debug_serverResponse = this.courseService.createCourse(this.corsoDaInserire);
-    debug_serverResponse.subscribe(this.notifica)
+    // debug_serverResponse.subscribe(this.notifica); //NON FUNZIONA PER IL SOLITO BINDING DEL THIS DI JAVASCRIPT
+    // debug_serverResponse.subscribe( n=> this.notifica(n) );
+    debug_serverResponse.subscribe( this.notifica.bind(this) );
+    console.log("debug_serverResponse:");
     console.log(debug_serverResponse);
 
     // alert("ragginta la fine di  conferma(myFirstNgForm :NgForm)");
   }
   notifica(notifica: CourseForInsert) {
+    console.log("XXX");
+    console.log(this);
+
+    console.log("NOTIFICA:");
     console.log(notifica);
+    this.router.navigate(["courses",notifica.idCorso]);
   }
 
 }
--- src/app/course-insert/courseForInsert.ts
+++ src/app/course-insert/courseForInsert.ts
@@ -1,10 +1,12 @@
 export interface ICourseForInsert{
+    idCorso :number,
     titolo :string,
     prezzoBase :number,
     difficolta :number
 }
 
 export class CourseForInsert implements ICourseForInsert{
+    idCorso :number;
     titolo: string;    
     prezzoBase: number;
     difficolta: number;   
