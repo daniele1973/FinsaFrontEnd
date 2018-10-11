780e8bdbee4b1cbafe0b688cf4a1cd12a675ba20
gianluca1989 <gianluca.virdis89@gmail.com>
2018-10-11 14:35:16 +0200

    Parte visualizzazione Corsi da modificare

---
+++ src/app/student-details/Corsi.ts
@@ -0,0 +1,6 @@
+export interface Corso{
+    idCorso: string;
+    titolo: string;
+    prezzoBase: string;
+    difficolta: string;
+}
--- src/app/student-details/student-details.component.ts
+++ src/app/student-details/student-details.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from './student-details';
 import { StudentDetailsService } from './student-details.service';
-import { Observable, Subscription } from "rxjs";
+import { Observable, Subscription, throwError } from "rxjs";
 import { ActivatedRoute, Router } from '@angular/router';
+import { Corso } from './Corsi';
+import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-student-details',
@@ -12,10 +15,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class StudentDetailsComponent implements OnInit {
 
   student : Student;
+  listCourse: Corso[] = [];
+  id: number;
   errorMessage : string = "tutto OK";
   private sub: Subscription;
+  studentFCUrl : string; 
+
+
   constructor(private route: ActivatedRoute,
-    private router: Router,private studentService: StudentDetailsService){
+    private router: Router,private studentService: StudentDetailsService, private http:HttpClient){
 
   }
 
@@ -23,13 +31,31 @@ export class StudentDetailsComponent implements OnInit {
 
     this.sub = this.route.params.subscribe(
       params => {
-        const id = +params['id'];
-        console.log("id -----> "+id);
-        this.studentService.getStudents(id).subscribe(
+        this.id = +params['id'];
+        console.log("id -----> "+this.id);
+        this.studentFCUrl = `http://localhost:56871/api/Allievi/${this.id}/corsi`;
+        this.studentService.getStudents(this.id).subscribe(
           instr => this.student = instr,
             err => this.errorMessage = <any>err
         );
       });
     }
+
+    DammiICorsi()
+    {
+      this.http.get<Corso[]>(this.studentFCUrl)
+            .pipe(
+                tap(d => console.log(JSON.stringify(d))),
+                catchError(this.handleError)
+            ).subscribe(
+              instr => this.listCourse = instr,
+              err => this.errorMessage = <any>err
+          ); 
+    }
+    handleError() {
+      alert("Errore di sbaglio!")
+      // throw new Error("Method not implemented.");
+      return throwError("Errore di sbaglio!");
+  }
 }
 
